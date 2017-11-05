e83c2465c333517f3a73874095dba4549992cadc
Ari Lotter <arilotter@gmail.com>
2017-11-05 11:20:15 -0500

    test

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.post("/call", (req, res) => {
     `${path.join(
       SIP_FOLDER,
       "sip"
-    )} | minimodem --rx -R 8000 -f input.wav 30`,
+    )} | minimodem --rx -R 8000 -f input.raw 30`,
     [],
     { shell: true }
   );
