9d80ff8e523cb66aef93c1a7cc01d38e1850969a
Ari Lotter <arilotter@gmail.com>
2017-11-07 10:41:48 -0500

    don't accept incoming calls

--- sip/sip.c
+++ sip/sip.c
@@ -6,27 +6,32 @@
 #define SIP_USER "sip"
 #define SIP_PASSWD "WildWildWest123"
 
-pjsua_recorder_id rec_id;
-pjsua_conf_port_id rec_port;
 
-/* Callback called by the library upon receiving incoming call */
-static void on_incoming_call(pjsua_acc_id acc_id, pjsua_call_id call_id, pjsip_rx_data *rdata)
+/* Display error and exit application */
+static void error_exit(const char *title, pj_status_t status)
 {
-    // this shouldn't happen
-    pjsua_call_info ci;
-
-    PJ_UNUSED_ARG(acc_id);
-    PJ_UNUSED_ARG(rdata);
-
-    pjsua_call_get_info(call_id, &ci);
-
-    PJ_LOG(3, (THIS_FILE, "Incoming call from %.*s!!",
-               (int)ci.remote_info.slen,
-               ci.remote_info.ptr));
-
-    /* Automatically answer incoming calls with 200/OK */
-    pjsua_call_answer(call_id, 200, NULL, NULL);
+    pjsua_perror(THIS_FILE, title, status);
+    pjsua_destroy();
+    exit(1);
 }
+
+
+ // helper for creating call-recorder
+static void create_recorder(pjsua_call_info ci)
+{
+	// specify target file
+	pj_str_t rec_file = pj_str("output.raw");
+	pj_status_t status = PJ_ENOTFOUND;
+	
+    pjsua_recorder_id rec_id = PJSUA_INVALID_ID;	
+	// Create recorder for call
+	status = pjsua_recorder_create(&rec_file, 0, NULL, 0, 0, &rec_id);
+	if (status != PJ_SUCCESS) error_exit("Error recording call", status);
+	
+	// connect active call to call recorder
+	pjsua_conf_port_id rec_port = pjsua_recorder_get_conf_port(rec_id);		
+	pjsua_conf_connect(ci.conf_slot, rec_port);
+	}
 
 /* Callback called by the library when call's state has changed */
 static void on_call_state(pjsua_call_id call_id, pjsip_event *e)
@@ -50,23 +55,8 @@ static void on_call_media_state(pjsua_call_id call_id)
 
     if (ci.media_status == PJSUA_CALL_MEDIA_ACTIVE)
     {
-        // When media is active, connect call to sound device.
-        // pjsua_conf_connect(ci.conf_slot, 0);
-        // pjsua_conf_connect(0, ci.conf_slot);
-        rec_id = PJSUA_INVALID_ID;
-        pj_str_t rec_raw_file = pj_str("output.raw");
-        pj_status_t status = pjsua_recorder_create(&rec_raw_file, 0, NULL, -1, 0, &rec_id);
-        rec_port = pjsua_recorder_get_conf_port(rec_id);
-        pjsua_conf_connect(ci.conf_slot, rec_port);
+        create_recorder(ci);
     }
-}
-
-/* Display error and exit application */
-static void error_exit(const char *title, pj_status_t status)
-{
-    pjsua_perror(THIS_FILE, title, status);
-    pjsua_destroy();
-    exit(1);
 }
 
 int main(int argc, char *argv[])
@@ -85,7 +75,6 @@ int main(int argc, char *argv[])
         pjsua_logging_config log_cfg;
 
         pjsua_config_default(&cfg);
-        cfg.cb.on_incoming_call = &on_incoming_call;
         cfg.cb.on_call_media_state = &on_call_media_state;
         cfg.cb.on_call_state = &on_call_state;
 
