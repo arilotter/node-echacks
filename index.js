bf346e0651b3824f4fe2d9f0aadc432160195103
Ari Lotter <arilotter@gmail.com>
2017-11-07 11:34:18 -0500

    test

--- index.js
+++ index.js
@@ -22,24 +22,25 @@ app.post("/call", (req, res) => {
   console.log(`New call from ${from}`);
   console.log("Starting SIP...");
 
-  const p = spawn(
-    `${path.join(
-      SIP_FOLDER,
-      "sip"
-    )}`, // | ffmpeg -hide_banner -loglevel panic -f alaw -i /dev/stdin -ar 8000 -ab 64000 -f ogg /dev/stdout | minimodem --rx -R 8000 -f input.ogg 30
-    [],
-    { shell: true }
-  );
+  // const p = spawn(
+  //   `${path.join(
+  //     SIP_FOLDER,
+  //     "sip"
+  //   )}`, // | ffmpeg -hide_banner -loglevel panic -f alaw -i /dev/stdin -ar 8000 -ab 64000 -f ogg /dev/stdout | minimodem --rx -R 8000 -f input.ogg 30
+  //   [],
+  //   { shell: true }
+  // );
+  const p = spawn("echo", ["test"]);
   p.stdout.on("data", data => {
     console.log(data.toString("utf8"));
   });
   p.stderr.on("data", data => {
     console.log(data.toString("utf8"));
   });
-  p.on('close', () => {
+  p.on("close", () => {
     console.log(`SIP for ${from} closed.`);
     delete calls[from];
-  })
+  });
   calls[from] = p;
 
   // Create TwiML response
